8d562ebdaf7e24f8d61bcbed8538a6721b617ed9
Tore Mæland Hirth <torehirth@gmail.com>
2024-05-29 08:39:29 +0200

    update backgr-img, font-colour, and start carousel

--- js/data/constants.mjs
+++ js/data/constants.mjs
@@ -1,3 +1,6 @@
 // ---mobile nav menu---
 export const hamburgerMenu = document.querySelector(".hamburger");
 export const mobileNav = document.querySelector(".mobile-nav");
+export const navLinks = document.querySelectorAll(".mobile-nav-link");
+export const mobileNavLinks = document.querySelector(".mobile-nav-links");
+export const mobileSoMeLinks = document.querySelectorAll(".mobile-social-media-links");
--- js/ui/toggleMobileNav.mjs
+++ js/ui/toggleMobileNav.mjs
@@ -1,13 +1,45 @@
-import { hamburgerMenu, mobileNav } from "../data/constants.mjs";
+import { hamburgerMenu, mobileNav, navLinks, mobileNavLinks, mobileSoMeLinks } from "../data/constants.mjs";
+
+// export function toggleMobileNav() {
+//   hamburgerMenu.addEventListener("click", function () {
+//     // toggles the hamburger menu and mobile nav.
+//     hamburgerMenu.classList.toggle("is-active");
+//     mobileNav.classList.toggle("is-active");
+//     // make the X white when mobile nav is displayed.
+//     if (mobileNav.classList.contains("is-active")) {
+//       hamburgerMenu.classList.add = "is-active";
+//     }
+//   });
+// }
 
 export function toggleMobileNav() {
-  hamburgerMenu.addEventListener("click", function () {
+  function closeMobileNav() {
+    hamburgerMenu.classList.remove("is-active");
+    mobileNav.classList.remove("is-active");
+  }
+
+  function handleClickOutside(event) {
+    if (!mobileNavLinks.contains(event.target) && !hamburgerMenu.contains(event.target)) {
+      closeMobileNav();
+    }
+  }
+
+  hamburgerMenu.addEventListener("click", function (event) {
+    // prevent the click event from propagating to the document
+    event.stopPropagation();
     // toggles the hamburger menu and mobile nav.
     hamburgerMenu.classList.toggle("is-active");
     mobileNav.classList.toggle("is-active");
-    // make the X white when mobile nav is displayed.
-    if (mobileNav.classList.contains("is-active")) {
-      hamburgerMenu.classList.add = "is-active";
-    }
+  });
+
+  document.addEventListener("click", handleClickOutside);
+
+  // close the nav when a page link inside it is clicked
+  navLinks.forEach((link) => {
+    link.addEventListener("click", closeMobileNav);
+  });
+  // close the nav when a SoMe link inside it is clicked
+  mobileSoMeLinks.forEach((link) => {
+    link.addEventListener("click", closeMobileNav);
   });
 }
