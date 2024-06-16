3a808ac1cf3ad8c876458e499a2b87f4f7683a8b
Tore Mæland Hirth <torehirth@gmail.com>
2024-06-16 11:33:23 +0200

    add latest section with cards on index, update carousel btn to 0 opacity

--- js/utils/updateCarouselButtonOpacity.mjs
+++ js/utils/updateCarouselButtonOpacity.mjs
@@ -1,17 +1,18 @@
 import { carousel, prevBtn, nextBtn } from "../data/constants.mjs";
 
 //function for updating the opacity of the carousel buttons
-prevBtn.style.opacity = 0.3; // set the opacity of the left button to 0.3 to start with.
+prevBtn.style.opacity = 0; // set the opacity of the left button to 0.3 to start with.
+
 export function updateCarouselButtonOpacity() {
   // left button
   if (carousel.scrollLeft <= 0) {
-    prevBtn.style.opacity = 0.3;
+    prevBtn.style.opacity = 0;
   } else {
     prevBtn.style.opacity = 1;
   }
   // right button
   if (carousel.scrollLeft >= carousel.scrollLeftMax) {
-    nextBtn.style.opacity = 0.3;
+    nextBtn.style.opacity = 0;
   } else {
     nextBtn.style.opacity = 1;
   }
