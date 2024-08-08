7e3ee12f514ace03d0a7aa69f552fd3ddd7c1bb8
Tore Mæland Hirth <torehirth@gmail.com>
2024-08-08 19:05:14 +0200

    update function for carousel button opacity

--- js/utils/updateCarouselButtonOpacity.mjs
+++ js/utils/updateCarouselButtonOpacity.mjs
@@ -1,7 +1,7 @@
 import { carousel, prevBtn, nextBtn } from "../data/constants.mjs";
 
 //function for updating the opacity of the carousel buttons
-prevBtn.style.opacity = 0; // set the opacity of the left button to 0.3 to start with.
+prevBtn.style.opacity = 0; // set the opacity of the left button to 0 to start with.
 
 export function updateCarouselButtonOpacity() {
   // left button
@@ -11,7 +11,7 @@ export function updateCarouselButtonOpacity() {
     prevBtn.style.opacity = 1;
   }
   // right button
-  if (carousel.scrollLeft >= carousel.scrollLeftMax) {
+  if (carousel.scrollWidth - carousel.clientWidth - carousel.scrollLeft <= 1) {
     nextBtn.style.opacity = 0;
   } else {
     nextBtn.style.opacity = 1;
