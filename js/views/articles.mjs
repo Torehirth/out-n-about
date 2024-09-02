1d4066a922482d233341b9c295272a8136e42b2d
Tore Mæland Hirth <torehirth@gmail.com>
2024-09-02 20:39:32 +0200

    add toggle function to category drop down button

--- js/views/articles.mjs
+++ js/views/articles.mjs
@@ -3,6 +3,7 @@ import { scrollToSectionByClick } from "../helper/scrollToSectionByClick.mjs";
 import { toTopButtonWrapper, headerContainer } from "../data/constants.mjs";
 import { handleCategoryButtons } from "../handler/handleCategoryButtons.mjs";
 import { fetchAllPosts } from "../api/fetchAllPosts.mjs";
+import { toggleCategoryDropDownButton } from "../handler/toggleCategoryDropDownButton.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
   // trying to increase loading speeds in terms of Lighthouse statistics
@@ -13,4 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
   scrollToSectionByClick(toTopButtonWrapper, headerContainer);
   // function to display all posts by category and change category by buttons
   handleCategoryButtons();
+  // toggle the category drop down button
+  toggleCategoryDropDownButton();
 });
