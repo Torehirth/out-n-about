3ea97026c8c515ad026b3a22987bd93cee9fdbd9
Tore Mæland Hirth <torehirth@gmail.com>
2024-09-03 18:57:33 +0200

    corrected errors in terms of html -and css validators, and website web accessibility test

--- js/handler/toggleCategoryDropDownButton.mjs
+++ js/handler/toggleCategoryDropDownButton.mjs
@@ -1,16 +1,23 @@
 const dropDownContainer = document.querySelector(".drop-down-container");
 const dropDownWrapper = document.querySelector(".drop-down-wrapper");
+const dropDownIcon = document.querySelector(".drop-down-btn span");
 
 export const toggleCategoryDropDownButton = () => {
   dropDownContainer.addEventListener("click", (e) => {
     // to stop event bubbling up or interfere with other elements
-    e.stopPropagation();
+    // e.stopPropagation();
     console.log(e.target);
 
     if (!dropDownWrapper.classList.contains("open")) {
+      // opens drop down menu
       dropDownWrapper.classList.add("open");
+      // makes the button icon change direction to up
+      dropDownIcon.textContent = "keyboard_arrow_up";
     } else {
+      // closes drop down menu
       dropDownWrapper.classList.remove("open");
+      // makes the button icon change direction to down
+      dropDownIcon.textContent = "keyboard_arrow_down";
     }
   });
 
