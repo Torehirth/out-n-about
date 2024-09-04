8f05555f145e9b7df468a0d32b6e31bd4d957ed5
Tore Mæland Hirth <torehirth@gmail.com>
2024-09-04 12:57:46 +0200

    add search bar and style, corrected some minor style faults

--- js/ui/updateCarouselButtonOpacity.mjs
+++ js/ui/updateCarouselButtonOpacity.mjs
@@ -1,23 +1,19 @@
 import { carousel, prevBtn, nextBtn } from "../data/constants.mjs";
 
 //function for updating the opacity of the carousel buttons
-prevBtn.style.opacity = 0; // set the opacity of the left button to 0 to start with.
-prevBtn.disabled = true;
+prevBtn.style.display = "none"; // hide the left button initially
 
 export function updateCarouselButtonOpacity() {
   // left button
   if (carousel.scrollLeft <= 0) {
-    prevBtn.style.opacity = 0;
+    prevBtn.style.display = "none";
   } else {
-    prevBtn.style.opacity = 1;
-    prevBtn.disabled = false;
+    prevBtn.style.display = "block";
   }
   // right button
   if (carousel.scrollWidth - carousel.clientWidth - carousel.scrollLeft <= 5) {
-    nextBtn.style.opacity = 0;
-    nextBtn.disabled = true;
+    nextBtn.style.display = "none";
   } else {
-    nextBtn.style.opacity = 1;
-    nextBtn.disabled = false;
+    nextBtn.style.display = "block";
   }
 }
--- js/views/index.mjs
+++ js/views/index.mjs
@@ -24,3 +24,57 @@ document.addEventListener("DOMContentLoaded", () => {
   // scroll to header by "to top button"
   scrollToSectionByClick(toTopButtonWrapper, headerContainer);
 });
+
+// ------- search bar event listeners ---------
+
+const searchForm = document.querySelector("#search-form");
+const searchInput = document.querySelector("#search-input");
+const navSearchButton = document.querySelector("#nav-search-btn");
+
+const toggleNavSearchIcon = () => {
+  // initially hidden
+  searchForm.classList.add("hidden");
+
+  navSearchButton.addEventListener("click", () => {
+    if (searchForm.classList.contains("hidden")) {
+      // if hidden, remove "hidden" class that displays search bar
+      searchForm.classList.remove("hidden");
+      // adds the transition effect on the search bar and sets the input in focus
+      setTimeout(() => {
+        searchForm.classList.add("visible");
+        searchInput.focus();
+      }, 10);
+    } else {
+      // add "hidden" class and remove "visible" class
+      searchForm.classList.add("hidden");
+      searchForm.classList.remove("visible");
+    }
+  });
+};
+
+toggleNavSearchIcon();
+
+const closeSearchBarByClick = () => {
+  document.addEventListener("click", (e) => {
+    // if the hidden class is not active, and the target localName is not a "span" or "input", the "hidden" class is added
+    if (searchForm.classList.contains("visible") && e.target.localName !== "span" && e.target.localName !== "input") {
+      searchForm.classList.add("hidden");
+    }
+  });
+};
+
+closeSearchBarByClick();
+
+const closeSearchBarByKey = (key) => {
+  document.addEventListener("keydown", (e) => {
+    // if the "hidden" class is not active and the called key is pressed, the "hidden" class is added
+    if (!searchForm.classList.contains("hidden") && e.key === key) {
+      searchForm.classList.add("hidden");
+    }
+  });
+};
+closeSearchBarByKey("Escape");
+
+// ----- search functionality -------
+const searchContainer = document.querySelector("#search-bar-container");
+const submitButton = document.querySelector("#search-submit-btn");
